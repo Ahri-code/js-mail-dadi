a96cc5dcdbbc78579206be53097c2903a8541808
Edy Galdiolo <edygaldy302@gmail.com>
2023-04-18 16:01:14 +0200

    Feat(js): for done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 
 const mail = ["pasquale@gmail.com", "alicia@gmail.com", "luca@libero.it", "angela@libero.it", "batili6425@raotus.com"];
 
+// --- VARIABLES ---
+
+let verify;
+let add;
+let i;
+let x;
+
+// --- INPUTS ---
+
+verify = prompt("Inserisci la tua mail");
+
+// --- CHECKS ---
+
+for (i = 0; i <= mail.length; i++) {
+    if(verify == mail[i]) {
+        x = 1;
+    }
+    else {
+        x = 2;
+    }
+    break;
+}
